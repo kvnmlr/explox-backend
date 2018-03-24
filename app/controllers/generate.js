814287203a602d845a6f8a6beada0f5b8c2d3ba8
kvnmlr <kevin.mueller1@live.de>
2018-03-24 23:01:23 +0100

    generate route form finished

--- app/controllers/generate.js
+++ app/controllers/generate.js
@@ -11,4 +11,12 @@ const Geo = mongoose.model('Geo');
 
 exports.generate = function (req, res) {
     res.render('loading', {text: "Routen werden gesucht"});
+    Log.log(TAG, "Used parameters: " + JSON.stringify(req.query));
+    var sport = req.query.sport || "cycling";
+    var distance = req.query.distance || "5.0";
+    var difficulty = req.query.difficulty || "advanced";
+    var start = {
+        lat: req.query.lat,
+        lng: req.query.lng
+    }
 };
