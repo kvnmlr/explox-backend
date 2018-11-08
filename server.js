94f35a931881cebafdd2d6162af0e3e1dcc77e9e
Kevin Müller <kevin.mueller1@live.de>
2018-11-08 17:28:00 +0100

    back to regular clusters

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const join = require('path').join;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-// const cluster = require('cluster');
+const cluster = require('cluster');
 const favicon = require('serve-favicon');
 const passport = require('passport');
 const config = require('./config');
@@ -16,7 +16,7 @@ const init = require('./init');
 const models = join(__dirname, 'app/models');
 
 const port = process.env.PORT || 3000;
-process.setMaxListeners(10);
+process.setMaxListeners(0);
 
 app.use(favicon(join(__dirname, 'public', 'favicon.ico')));
 
@@ -37,17 +37,16 @@ require('./config/passport')(passport);
 require('./config/express')(app, passport);
 require('./config/routes')(app, passport);
 
-if (/* cluster.isMaster && */app.get('env') === 'development') {
+if (cluster.isMaster) {
     Log.log(TAG, '_____Starting Server_____');
     Log.log(TAG, 'Starting ' + numCPUs + ' workers on port ' + port);
 
     connect()
         .on('error', console.log)
         .on('disconnected', connect)
-        // .once('open', initialize);
-        .once('open', listen);
+        .once('open', initialize);
 
-    /* if (app.get('env') === 'development') {
+    if (app.get('env') !== 'test') {
         for (let i = 0; i < numCPUs; i++) {
             cluster.fork();
         }
@@ -65,7 +64,6 @@ if (/* cluster.isMaster && */app.get('env') === 'development') {
         Log.error(TAG, 'Worker ' + deadWorker.process.pid + ' has died');
         Log.log(TAG, 'Worker ' + worker.process.pid + ' was born');
     });
-    */
 
 } else {
     connect()
