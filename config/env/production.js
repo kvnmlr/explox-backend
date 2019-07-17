b7f76879d98a113d402933fd2e8ee56229dfd532
kvnmlr <kevin.mueller1@live.de>
2019-07-17 15:19:43 +0200

    no https auth callback

--- config/env/production.js
+++ config/env/production.js
@@ -3,13 +3,13 @@
 module.exports = {
     env: 'production',
     db: 'mongodb://localhost/exploxdb_prod',
-    frontend_url: 'https://umtl.dfki.de/explox/',
+    frontend_url: 'http://umtl.dfki.de/explox/',
     email: process.env.EMAIL,
     email_password: process.env.EMAIL_PASSWORD,
     mapbox_token: process.env.MAPBOX_TOKEN,
     strava: {
         clientID: process.env.STRAVA_CLIENTID,
         clientSecret: process.env.STRAVA_SECRET,
-        callbackURL: 'https://umtl.dfki.de/explox/backend/auth/strava/callback'
+        callbackURL: 'http://umtl.dfki.de/explox/backend/auth/strava/callback'
     },
 };
